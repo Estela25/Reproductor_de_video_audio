0b09b0a524d139a84f07f325b9294579e5c1e655
Estela25 <USIS035821@ugb.edu.sv>
2022-02-17 18:14:16 -0500

    Diseño,strings,java

---
+++ app/src/main/java/com/example/reproducir_audio_y_vdeo_grupo4/Camara.java
@@ -0,0 +1,14 @@
+package com.example.reproducir_audio_y_vdeo_grupo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Camara extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara);
+    }
+}
