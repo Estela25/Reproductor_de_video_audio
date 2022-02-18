bc2dc3bbd03b35f346b8dbbf0570bf83f0690673
Rmejia39 <sortomejia66@gmail.com>
2022-02-18 01:42:24 +0100

    codigo de video

--- app/src/main/java/com/example/reproducir_audio_y_vdeo_grupo4/Video.java
+++ app/src/main/java/com/example/reproducir_audio_y_vdeo_grupo4/Video.java
@@ -1,8 +1,9 @@
 package com.example.reproducir_audio_y_vdeo_grupo4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class Video extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class Video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        String path = "androi.resource://"+ getPackageName() + "/" +
+                R.raw.rawr;
+        videoView.setVideoURI(Uri.parse(path));
+        videoView.start();
     }
 }
