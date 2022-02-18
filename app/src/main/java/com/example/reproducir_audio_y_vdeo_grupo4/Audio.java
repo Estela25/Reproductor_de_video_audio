f7e1882a2eaf79f4a8c3026e937be24c13900f9f
Estela25 <USIS035821@ugb.edu.sv>
2022-02-18 16:29:14 -0500

    Boton volver

--- app/src/main/java/com/example/reproducir_audio_y_vdeo_grupo4/Audio.java
+++ app/src/main/java/com/example/reproducir_audio_y_vdeo_grupo4/Audio.java
@@ -2,7 +2,9 @@ package com.example.reproducir_audio_y_vdeo_grupo4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Audio extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Audio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
+    }
+
+
+    public void regresar(View view) {
+        Intent volver = new Intent(this, MainActivity.class);
+        startActivity(volver);
     }
 }
